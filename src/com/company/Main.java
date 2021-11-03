9abfa1e48873603596b4b70e93d88a45a08e6d38
Acer <nastyakharchenko23@gmail.com>
2021-11-03 10:45:36 +0200

    LR4

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Character.toString(0x1F602) +
+                Character.toString(0x1F608) +
+                Character.toString(0x1F634));
+        String str="7.3";
+        String text = "2.34"; // example String
+        System.out.println(Double.parseDouble(str)+Double.parseDouble(text));
+    }
+}
